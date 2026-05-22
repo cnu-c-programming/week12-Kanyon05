3a0def46d88ab4aa74493cfc4c83b9cf0b1fb9a7
Kanyon05 <windtree01@gmail.com>
2026-05-22 11:54:05 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[]) {
     const char* filename = "numbers.txt";
-
     FILE* fp = fopen(filename, "r");
-    if (fp == NULL) {
-        return 0;
-    }
-
+    if (fp == NULL) return 0;
 
     int sum = 0;
-
+    int num;
+    while (fscanf(fp, "%d", &num) == 1) {
+        sum += num;
+    }
+    if (ferror(fp)) {
+        fprintf("invalid input", stderr, "\n");
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
