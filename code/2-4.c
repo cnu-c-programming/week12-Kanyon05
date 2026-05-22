96d114b730acc950aa4e7e6a73958a479049fa38
Kanyon05 <windtree01@gmail.com>
2026-05-22 11:25:31 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2)
-        return 0;
+    if (argc < 2) return 0;
 
     FILE* fp = fopen(argv[1], "r");
 
-
-
+    for (char c= 'a'; c <= 'z'; c++) {
+        int count = 0;
+        for (char d; (d = fgetc(fp)) != EOF; ) {
+            if (d == c) count++;
+        }
+        printf("%c: %d\n", c, count);
+        rewind(fp);
+    }
+    
     fclose(fp);
 }
-
