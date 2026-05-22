4962577fe0505684d3a32893f01aa52b6d80b70d
Kanyon05 <windtree01@gmail.com>
2026-05-22 11:28:24 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 3)
-        return 0;
+    if (argc<3) return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    char c;
+    while ((c = fgetc(fp_src)) != EOF) {
+        fputc(c, fp_dst);
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
 }
-
