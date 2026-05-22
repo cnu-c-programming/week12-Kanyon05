a99b3e093f012860610ff912fa9f3527ec8a32aa
Kanyon05 <windtree01@gmail.com>
2026-05-22 11:42:49 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 2) 
-        return 0;
-    
+int main(int argc, const char *argv[]) {
+    if (argc < 2) return 0;
+
     FILE* fp = fopen(argv[1], "r");
-
-
+    if (fp == NULL) {
+        perror("file dose not exists");
+        return 1;
+    }
     return 0;
 }
